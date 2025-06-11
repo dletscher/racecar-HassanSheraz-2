6c4f27c9807d6dfd2fdccf2b85a3b8f3116536cc
Hassan Sheraz <hsnshrz@gmail.com>
2025-06-10 19:30:21 -0500

    RuleAgent.py

--- RuleAgent.py
+++ RuleAgent.py
@@ -1,5 +1,45 @@
 import random
 
 class Agent:
-	def chooseAction(self, observations, possibleActions):
-		return ('straight', 'coast')	
+    def chooseAction(self, observations, possibleActions):
+        lidar = observations['lidar'] 
+        velocity = observations['velocity']
+
+        left, front_left, front, front_right, right = lidar
+
+        obstacle_threshold = 2.0
+        slow_velocity = 1.0
+        high_velocity = 3.0
+
+        steering = 'straight'
+        throttle = 'coast'
+
+        if front < obstacle_threshold:
+            # Obstacle ahead — decide to turn
+            if front_left > front_right:
+                steering = 'left'
+            else:
+                steering = 'right'
+
+            if velocity > slow_velocity:
+                throttle = 'brake'
+            else:
+                throttle = 'coast'
+        else:
+            if left > right:
+                steering = 'left'
+            elif right > left:
+                steering = 'right'
+            else:
+                steering = 'straight'
+
+            if velocity < high_velocity:
+                throttle = 'accelerate'
+            else:
+                throttle = 'coast'
+
+        action = (steering, throttle)
+        if action in possibleActions:
+            return action
+        else:
+            return ('straight', 'coast')
